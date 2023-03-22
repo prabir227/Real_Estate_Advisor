a5604c0ea9f4a2abe2486b831aeaa755b0fe06ec
Prabir Ghosh <prabirghosh227@gmail.com>
2023-03-22 11:01:14 +0530

    Added code to connect mysql database

---
+++ Real_Estate_Advisor.c
@@ -0,0 +1,36 @@
+#include <mysql.h>
+#include <stdio.h>
+
+int main() {
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
+    char *server = "localhost";
+    char *user = "username";
+    char *password = "password";
+    char *database = "database_name";
+
+    conn = mysql_init(NULL);
+
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    if (mysql_query(conn, "SELECT * FROM table_name")) {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    res = mysql_use_result(conn);
+
+    while ((row = mysql_fetch_row(res)) != NULL) {
+        printf("%s %s %s\n", row[0], row[1], row[2]);
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+
+    return 0;
+}

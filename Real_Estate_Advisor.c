77420954935d9062543f2afd6c839a0a56178e00
Prabir Ghosh <prabirghosh227@gmail.com>
2023-03-23 19:13:38 +0530

    added documentation to the insert function

--- Real_Estate_Advisor.c
+++ Real_Estate_Advisor.c
@@ -3,9 +3,11 @@
 #include <string.h>
 int insert(MYSQL *conn)
 {
+    //initializing the required variables.
     int P_id, bhk, price;
     char Desired_Area[10], locality[40], P_name[50];
     char query[200];
+    //Taking all the required data from user.
     printf("Enter property id: ");
     scanf("%d", &P_id);
     printf("Enter area of your property: ");
@@ -13,16 +15,20 @@ int insert(MYSQL *conn)
     scanf("%[^\n]%*c",Desired_Area);
     printf("Enter number of BHK: ");
     scanf("%d", &bhk);
-    printf("ENter locality of your property: ");
+    printf("Enter locality of your property: ");
     scanf("\n");
     scanf("%[^\n]%*c",locality);
-    printf("Enter name of your property");
+    printf("Enter name of your property: ");
     scanf("\n");
     scanf("%[^\n]%*c",P_name);
-    printf("Enter price of ypur property");
+    printf("Enter price of ypur property: ");
     scanf("%d", &price);
+    //creating the query variable by adding the required sql query and the variables entered by the user.
     sprintf(query, "INSERT INTO property (p_id, Desired_Area, bhk, locality, P_name, price) VALUES(%d, '%s', %d, '%s', '%s', %d)",
             P_id, Desired_Area, bhk, locality, P_name, price);
+    //Inserting values to the mysql database.
+    //The mysql_query function returns '0' when executed successfully.
+    //So if it does not return zero then the required error will be printed on the screen.
     if (mysql_query(conn, query))
     {
         fprintf(stderr, "%s\n", mysql_error(conn));
